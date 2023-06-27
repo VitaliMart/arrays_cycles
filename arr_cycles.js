77b9bd9f18cd110f237e6abbd79713bd2b3c8955
Vitali Martsinchyk <vitali.martinchik@gmail.com>
2023-06-27 22:55:24 +0300

    feat: edit

--- arr_cycles.js
+++ arr_cycles.js
@@ -1,10 +1,8 @@
 // 1. Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
-// Дан массив, состоящий из названий фильмов, перебрать массив с выводом названий каждого фильма в консоль
 const arrMovie = ['King', 'Interpreter', 'Taxi', 'Fauda'];
 arrMovie.forEach(el => console.log(el));
-// 2. Given an array of car manufacturers, convert the array to a string and back to an array
-// Учитывая массив производителей автомобилей, преобразовать массив в строку и обратно в массив
 
+// 2. Given an array of car manufacturers, convert the array to a string and back to an array
 const carManufacturer = ['Honda', 'Ford', 'Nissan', 'Audi', 'Toyota',];
 let carM = carManufacturer.join();
 console.log(carM);
@@ -22,31 +20,27 @@ let arr2 = arr.map(el => new Boolean(el));
 console.log(arr2);
 
 // 5.Sort the array [1,6,7,8,3,4,5,6] in descending order
-// Отсортируйте массив [1,6,7,8,3,4,5,6] в порядке убывания
 const sortArr = [1, 6, 7, 8, 3, 4, 5, 6];
 sortArr.sort((b, a) => a - b);
 console.log(sortArr);
 
 // 6. Filter array [1,6,7,8,3,4,5,6] by value> 3
-// Массив фильтров [1,6,7,8,3,4,5,6] по значению> 3
 const arrNum = [1, 6, 7, 8, 3, 4, 5, 6];
 const filterArr = arrNum.filter(el => el > 3);
 console.log(filterArr);
 
 // 7. Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
-//  Напишите функцию, которая принимает два параметра — массив и число и выводит индекс элемента массива, равный числу
 function indexElmentArrya(array, number) {
     let result;
     return result = array.forEach((el, ind) => el == number ? console.log(ind) : '');
 };
-indexElmentArrya(arrNum, 3);
+
 // 8. Implement a loop that will print the number 'a' until it is less than 10
-// Реализовать цикл, который будет печатать число «а», пока оно не станет меньше 10
 for (let i = 12; i >= 10; i--) {
     console.log(i);
 };
+
 // 9. Implement a loop that prints prime numbers to the console
-// Реализовать цикл, выводящий на консоль простые числа.
 let num = 7;
 cont: for (let i = 2; i <= num; i++) {
     for (let k = 2; k < i; k++) {
@@ -56,8 +50,8 @@ cont: for (let i = 2; i <= num; i++) {
     };
     console.log(i);
 };
+
 // 10. Implement a loop that prints odd numbers to the console
-// Реализовать цикл, выводящий на консоль нечетные числа.
 let n = 8;
 for (let i = 0; i <= n; i++) {
     if (i % 2 != 0) {
